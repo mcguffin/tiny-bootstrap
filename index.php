3a66c506a9bca7c593fd03b6e7f0c874be59f941
mcguffin <joern@flyingletters.com>
2018-09-11 15:10:48 +0200

    Release version 0.1.0

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ Plugin Name: Tiny Bootstrap
 Plugin URI: http://wordpress.org/
 Description: Enter description here.
 Author: Jörn Lund
-Version: 0.0.7
+Version: 0.1.0
 Author URI: https://github.com/mcguffin
 License: GPL3
 Github Repository: mcguffin/tiny-bootstrap
